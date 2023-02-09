52a4f73ff669913de545397cd89d7c4cad3565bd
Danny Nguyen <dnguyen130@my.bcit.ca>
2023-02-08 19:49:39 -0800

    small styling changes

--- src/components/desktop/addbutton/index.tsx
+++ src/components/desktop/addbutton/index.tsx
@@ -1,11 +1,15 @@
+import { FaPlus } from 'react-icons/fa'
 import { useTheme } from '@utils/provider'
+import { IconContext } from 'react-icons'
 
 export default function AddButtonDesktop() {
   const { theme } = useTheme()
 
   return (
     <button className={`addbuttondesktop-${theme}`}>
-      <div className={`addicondesktop-${theme}`}>+</div>
+      <IconContext.Provider value={{ className: `addicondesktop-${theme}` }}>
+        <FaPlus size="100%" />
+      </IconContext.Provider>
     </button>
   )
 }
