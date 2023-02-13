8c8e3b70357bbca0cb25995978d4b565785c1c1e
Danny Nguyen <dnguyen130@my.bcit.ca>
2023-02-12 22:01:09 -0800

    eslint update, fix ts issues

--- src/App.tsx
+++ src/App.tsx
@@ -7,44 +7,44 @@ import Navbar from '@components/desktop/navbar'
 import Navbarmobile from '@/components/mobile/navbar'
 import AddButtonDesktop from '@components/desktop/addbutton'
 
-const AnimatedOutlet = () => {
+const AnimatedOutlet = (): React.ReactElement => {
   const o = useOutlet()
   const [outlet] = useState(o)
   return <>{outlet}</>
 }
 
-export default function App() {
+export default function App(): JSX.Element {
   const { theme } = useTheme()
   const location = useLocation()
 
-  if (true) {
-    return <>login</>
-  }
+  // if (true) {
+  //   return <>login</>
+  // }
 
-  if (false) {
-    return (
-      <>
-        <Navbar />
-        <Navbarmobile />
-        <AddButtonDesktop />
-        <section className={`container-${theme}`}>
-          <AnimatePresence mode="wait" initial={false}>
-            <motion.div
-              key={location.pathname}
-              className="layout"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{
-                type: 'linear',
-                duration: 0.2,
-              }}
-            >
-              <AnimatedOutlet />
-            </motion.div>
-          </AnimatePresence>
-        </section>
-      </>
-    )
-  }
+  // if (false) {
+  return (
+    <>
+      <Navbar />
+      <Navbarmobile />
+      <AddButtonDesktop />
+      <section className={`container-${theme}`}>
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.div
+            key={location.pathname}
+            className="layout"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{
+              type: 'linear',
+              duration: 0.2,
+            }}
+          >
+            <AnimatedOutlet />
+          </motion.div>
+        </AnimatePresence>
+      </section>
+    </>
+  )
+  // }
 }
