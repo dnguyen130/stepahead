38bc6a5965f48a3cf4662e7679c59e2603a793cd
Danny Nguyen <dnguyen130@my.bcit.ca>
2023-03-16 23:41:18 -0700

    modal progress

--- src/components/shared/createTask/index.tsx
+++ src/components/shared/createTask/index.tsx
@@ -1,9 +1,10 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import { useMyContext } from '@/utils/provider'
 import { AnimatePresence, motion } from 'framer-motion'
 
 export default function CreateTask(): ReactElement {
   const { activeModal, setActiveModal } = useMyContext()
+  const [activeTab, setActiveTab] = useState('event')
 
   return (
     <AnimatePresence mode="wait">
@@ -18,14 +19,43 @@ export default function CreateTask(): ReactElement {
             duration: 0.2,
           }}
         >
-          Hello
-          <button
-            onClick={() => {
-              setActiveModal(false)
-            }}
-          >
-            Off
-          </button>
+          <header className="createtaskheader">
+            <button
+              className={
+                activeTab === 'event'
+                  ? 'createtasktab-active'
+                  : 'createtasktab-inactive'
+              }
+              onClick={() => {
+                setActiveTab('event')
+              }}
+            >
+              Create Event
+            </button>
+            <button
+              className={
+                activeTab === 'journal'
+                  ? 'createtasktab-active'
+                  : 'createtasktab-inactive'
+              }
+              onClick={() => {
+                setActiveTab('journal')
+              }}
+            >
+              Create Journal Entry
+            </button>
+          </header>
+
+          <div className="createtask-content">
+            {' '}
+            <button
+              onClick={() => {
+                setActiveModal(false)
+              }}
+            >
+              Off
+            </button>
+          </div>
         </motion.div>
       )}
     </AnimatePresence>
